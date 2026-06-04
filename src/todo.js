e432646da6a90526572b9debeaf03abe15c0e706
Oliver Henwood <henwoodo97@gmail.com>
2026-06-04 14:29:33 +0100

    Setting up project / repository. Initial commit.

---
+++ src/todo.js
@@ -0,0 +1,7 @@
+export function createToDo(title, description, dateDue, priority, completed){
+    this.title = title;
+    this.description = description;
+    this.dateDue = dateDue;
+    this.priority = priority;
+    this.completed = completed;
+}
